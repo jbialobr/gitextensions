b91252f52b572d9bce72c40226121331cf76c9f3
gpongelli <gabriele.pongelli@datalogic.com>
2017-09-21 15:22:16 +0200

    Fix about parent class, newline and translation.

--- GitUI/FormSignatureInfo.cs
+++ GitUI/FormSignatureInfo.cs
@@ -7,19 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GitCommands.Utils;
 
 namespace GitUI
 {
-    public partial class FormSignatureInfo : Form
+    public partial class FormSignatureInfo : GitExtensionsForm
     {
         public FormSignatureInfo()
         {
             InitializeComponent();
+            Translate();
         }
 
         public void ShowGpgMessage(string message)
         {
-            gpgTextBox.Text = message;
+            gpgTextBox.Text = EnvUtils.ReplaceLinuxNewLinesDependingOnPlatform(message);
         }
 
         private void button1_Click(object sender, EventArgs e)
