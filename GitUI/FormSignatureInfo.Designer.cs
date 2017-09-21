37c1e990e6776456520df7a408581dd3f1c99e43
gpongelli <gabriele.pongelli@datalogic.com>
2017-09-21 15:21:40 +0200

    Added a table layout to correctly show the window

--- GitUI/FormSignatureInfo.Designer.cs
+++ GitUI/FormSignatureInfo.Designer.cs
@@ -31,14 +31,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSignatureInfo));
             this.closeBtn = new System.Windows.Forms.Button();
             this.gpgTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // closeBtn
             // 
-            this.closeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.closeBtn.Location = new System.Drawing.Point(329, 87);
+            this.closeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeBtn.Location = new System.Drawing.Point(256, 86);
             this.closeBtn.Name = "closeBtn";
-            this.closeBtn.Size = new System.Drawing.Size(116, 26);
+            this.closeBtn.Size = new System.Drawing.Size(116, 29);
             this.closeBtn.TabIndex = 1;
             this.closeBtn.Text = "Close";
             this.closeBtn.UseVisualStyleBackColor = true;
@@ -50,30 +53,48 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gpgTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.gpgTextBox.Location = new System.Drawing.Point(12, 12);
+            this.gpgTextBox.Location = new System.Drawing.Point(3, 3);
             this.gpgTextBox.Multiline = true;
             this.gpgTextBox.Name = "gpgTextBox";
             this.gpgTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.gpgTextBox.Size = new System.Drawing.Size(433, 69);
+            this.gpgTextBox.Size = new System.Drawing.Size(369, 77);
             this.gpgTextBox.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.gpgTextBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.closeBtn, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(375, 118);
+            this.tableLayoutPanel1.TabIndex = 3;
             // 
             // FormSignatureInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(457, 125);
-            this.Controls.Add(this.gpgTextBox);
-            this.Controls.Add(this.closeBtn);
+            this.ClientSize = new System.Drawing.Size(397, 142);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormSignatureInfo";
             this.Text = "GPG Info";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button closeBtn;
         private System.Windows.Forms.TextBox gpgTextBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
