214771bd36b74610e076aa0e66ea2353e0966dcb
Eben Zhang <zhangyingneng@gmail.com>
2018-03-03 21:51:50 +0800

    fixup! revert changes to the WarningToolStripItem
    don't why it was changed by the original author.

--- GitUI/UserControls/WarningToolStripItem.cs
+++ GitUI/UserControls/WarningToolStripItem.cs
@@ -1,65 +1,39 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace GitUI
 {
-    /// <summary><see cref="ToolStripButton"/> which blinks when</summary>
     public class WarningToolStripItem : ToolStripButton
     {
-        private readonly Timer _blinkTimer = new Timer { Interval = 150 };
+        private readonly Timer _blinkTimer;
         private int _counter;
-        private readonly Color _offColor;
 
         public WarningToolStripItem()
         {
+            _counter = 0;
             Width = 200;
             Height = 20;
-
-            _offColor = Parent?.BackColor ?? SystemColors.Control;
-            _blinkTimer.Tick += BlinkOnTick;
-
+            _blinkTimer = new Timer {Interval = 150, Enabled = true};
+            _blinkTimer.Tick += _blinkTimer_Tick;
             _blinkTimer.Start();
         }
 
-        private void BlinkOnTick(object s, EventArgs e)
+        void _blinkTimer_Tick(object sender, EventArgs e)
         {
-             ToggleOnOff(() =>
-             {
-                 BackColor = Color.Salmon;
-             }, () =>
-             {
-                 BackColor = _offColor;
-             });
-        }
-
-        private void ToggleOnOff(Action on, Action off, Action finish = null)
-        {
-            if (_counter % 2 == 0)
-            {// even -> off
-                off();
-                Debug.WriteLine("off");
-            }
+            if (BackColor == Color.Salmon && Parent != null)
+                BackColor = Parent.BackColor;
             else
-            {// odd -> on
-                on();
-                Debug.WriteLine("on");
-            }
+                BackColor = Color.Salmon;
 
             _counter++;
 
-            if (_counter >= 20)
-            {// enough blinks -> stop blinking
-                Debug.WriteLine(">stop");
+            if (_counter > 20)
+            {
                 _blinkTimer.Stop();
-                _blinkTimer.Tick -= BlinkOnTick;
-                if (finish != null)
-                {
-                    finish();
-                }
-                Debug.WriteLine(">done");
+                BackColor = Color.Salmon;
             }
         }
+
     }
 }
