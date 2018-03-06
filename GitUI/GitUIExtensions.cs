jbialobr/gitextensions

commit 1dad241ef72edd3eefe86ff35032ef2c48f8a0e9

Browse Diff presentation of Untracked files (#4313)
Reported in #4301, some explanations there
This mostly drops some special checks, handling untracked with git-diff --no-index
    - `if (parentRevision == null)` in ViewChanges() were never taken. If so, it would have displayed the untracked similar to FormCommit. Using ViewFile as done in FormCommit would have been cleaner and better presentation. For instance,  `FileReader.ReadFileContent(fullPath, diffViewer.Encoding)` will not check for binary files and will present garbage or give exceptions.
    - `IsItemUntracked()` was slightly incorrect (should have used `file.IsTracked`)

* Move handling of git-diff untracked files to RevisionDiffProvider, add tests
  Handling of untracked also added to OpenWithDiffTool, the option should normally be blocked though in the UI

* Rename RevisionDiffProvider parameters from/parentRev -> firstRevision, to/revB -> secondRevision

* Reordered arguments for ViewChanges to be (firstRevision, secondRevision)

GitUI/GitUIExtensions.cs
@@ -18,7 +18,7 @@ namespace GitUI
         public static SynchronizationContext UISynchronizationContext;
 
 
-        public static void OpenWithDifftool(this RevisionGrid grid, string fileName, string oldFileName, GitUI.RevisionDiffKind diffKind)
+        public static void OpenWithDifftool(this RevisionGrid grid, string fileName, string oldFileName, GitUI.RevisionDiffKind diffKind, bool isTracked=true)
         {
             //Note: Order in revisions is that first clicked is last in array
             string extraDiffArgs;
@@ -32,46 +32,29 @@ namespace GitUI
             }
             else
             {
-                string output = grid.Module.OpenWithDifftool(fileName, oldFileName, firstRevision, secondRevision, extraDiffArgs);
+                string output = grid.Module.OpenWithDifftool(fileName, oldFileName, firstRevision, secondRevision, extraDiffArgs, isTracked);
                 if (!string.IsNullOrEmpty(output))
                     MessageBox.Show(grid, output);
             }
-        }
-
-        public static bool IsItemUntracked(GitItemStatus file,
-            string firstRevision, string secondRevision)
-        {
-            if (firstRevision == GitRevision.UnstagedGuid && file.IsDeleted ||
-                secondRevision == GitRevision.UnstagedGuid && file.IsNew)
-            {
-                return true;
-            }
-            else if (firstRevision == GitRevision.UnstagedGuid) //working directory changes
-            {
-                if (secondRevision == null || secondRevision == GitRevision.IndexGuid)
-                    return !file.IsTracked;
-            }
-            return false;
         }
 
         private static PatchApply.Patch GetItemPatch(GitModule module, GitItemStatus file,
             string firstRevision, string secondRevision, string diffArgs, Encoding encoding)
         {
             return module.GetSingleDiff(firstRevision, secondRevision, file.Name, file.OldName,
-                    diffArgs, encoding, true);
+                    diffArgs, encoding, true, file.IsTracked);
         }
 
         public static string GetSelectedPatch(this FileViewer diffViewer, string firstRevision, string secondRevision, GitItemStatus file)
         {
-            if (firstRevision == null)
-                return null;
-
-            if (IsItemUntracked(file, firstRevision, secondRevision))
+            if (!file.IsTracked)
             {
                 var fullPath = Path.Combine(diffViewer.Module.WorkingDir, file.Name);
                 if (Directory.Exists(fullPath) && GitModule.IsValidGitWorkingDir(fullPath))
+                {
+                    //git-status does not detect details for untracked and git-diff --no-index will not give info
                     return LocalizationHelpers.GetSubmoduleText(diffViewer.Module, file.Name.TrimEnd('/'), "");
-                return FileReader.ReadFileContent(fullPath, diffViewer.Encoding);
+                }
             }
 
             if (file.IsSubmodule && file.SubmoduleStatus != null)
@@ -90,34 +73,24 @@ namespace GitUI
 
         public static void ViewChanges(this FileViewer diffViewer, IList<GitRevision> revisions, GitItemStatus file, string defaultText)
         {
-            var firstRevision = revisions.Count > 0 ? revisions[0] : null;
-            string firstRevisionGuid = firstRevision == null ? null : firstRevision.Guid;
-            string parentRevisionGuid = revisions.Count == 2 ? revisions[1].Guid : null;
-            if (parentRevisionGuid == null && firstRevision != null)
-                parentRevisionGuid = firstRevision.FirstParentGuid;
-            ViewChanges(diffViewer, firstRevisionGuid, parentRevisionGuid, file, defaultText);
+            if (revisions.Count == 0)
+                return;
+
+            var selectedRevision = revisions[0];
+            string secondRevision = selectedRevision?.Guid;
+            string firstRevision = revisions.Count >= 2 ? revisions[1].Guid : null;
+            if (firstRevision == null && selectedRevision != null)
+                firstRevision = selectedRevision.FirstParentGuid;
+            ViewChanges(diffViewer, firstRevision, secondRevision, file, defaultText);
         }
 
-        public static void ViewChanges(this FileViewer diffViewer, string revision, string parentRevision, GitItemStatus file, string defaultText)
+        public static void ViewChanges(this FileViewer diffViewer, string firstRevision, string secondRevision, GitItemStatus file, string defaultText)
         {
-            if (parentRevision == null)
+            diffViewer.ViewPatch(() =>
             {
-                if (file.TreeGuid.IsNullOrEmpty())
-                    diffViewer.ViewGitItemRevision(file.Name, revision);
-                else if (!file.IsSubmodule)
-                    diffViewer.ViewGitItem(file.Name, file.TreeGuid);
-                else
-                    diffViewer.ViewText(file.Name,
-                        LocalizationHelpers.GetSubmoduleText(diffViewer.Module, file.Name, file.TreeGuid));
-            }
-            else
-            {
-                diffViewer.ViewPatch(() =>
-                    {
-                        string selectedPatch = diffViewer.GetSelectedPatch(parentRevision, revision, file);
-                        return selectedPatch ?? defaultText;
-                    });
-            }
+                string selectedPatch = diffViewer.GetSelectedPatch(firstRevision, secondRevision, file);
+                return selectedPatch ?? defaultText;
+            });
         }
 
         public static void RemoveIfExists(this TabControl tabControl, TabPage page)

GitCommands/Git/GitModule.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -2213,7 +2213,7 @@ namespace GitCommands
             return stashes;
         }
 
-        public Patch GetSingleDiff(string @from, string to, string fileName, string oldFileName, string extraDiffArguments, Encoding encoding, bool cacheResult)
+        public Patch GetSingleDiff(string @from, string to, string fileName, string oldFileName, string extraDiffArguments, Encoding encoding, bool cacheResult, bool isTracked=true)
         {
             if (!string.IsNullOrEmpty(fileName))
             {
@@ -2225,15 +2225,14 @@ namespace GitCommands
             }
 
             //fix refs slashes
-            from = from.ToPosixPath();
-            to = to == null ? "":to.ToPosixPath();
-            string commitRange = _revisionDiffProvider.Get(from, to);
+            from = from == null ? "" : from.ToPosixPath();
+            to = to == null ? "" : to.ToPosixPath();
+            string diffOptions = _revisionDiffProvider.Get(from, to, fileName, oldFileName, isTracked);
             if (AppSettings.UsePatienceDiffAlgorithm)
                 extraDiffArguments = string.Concat(extraDiffArguments, " --patience");
 
             var patchManager = new PatchManager();
-            var arguments = String.Format(DiffCommandWithStandardArgs + "{0} -M -C {1} -- {2} {3}", extraDiffArguments, commitRange,
-                fileName.Quote(), oldFileName.Quote());
+            var arguments = String.Format(DiffCommandWithStandardArgs + "{0} -M -C {1}", extraDiffArguments, diffOptions);
             cacheResult = cacheResult && !GitRevision.IsArtificial(to) && !GitRevision.IsArtificial(from) && !to.IsNullOrEmpty() && !from.IsNullOrEmpty();
             string patch;
             if (cacheResult)
@@ -3017,6 +3016,7 @@ namespace GitCommands
         {
             if (revision == GitRevision.UnstagedGuid) //working directory changes
             {
+                Debug.Assert(false, "Tried to get blob for unstaged file");
                 return null;
             }
             if (revision == GitRevision.IndexGuid) //index
@@ -3096,11 +3096,11 @@ namespace GitCommands
                 });
         }
 
-        public string OpenWithDifftool(string filename, string oldFileName = "", string revision1 = null, string revision2 = null, string extraDiffArguments = "")
+        public string OpenWithDifftool(string filename, string oldFileName = "", string revision1 = null, string revision2 = null, string extraDiffArguments = "", bool isTracked=true)
         {
             var output = "";
 
-            string args = string.Join(" ", extraDiffArguments, _revisionDiffProvider.Get(revision1, revision2), "--", filename.QuoteNE(), oldFileName.QuoteNE());
+            string args = string.Join(" ", extraDiffArguments, _revisionDiffProvider.Get(revision1, revision2, filename, oldFileName, isTracked));
             RunGitCmdDetached("difftool --gui --no-prompt " + args);
             return output;
         }

GitCommands/Git/RevisionDiffProvider.cs
@@ -7,12 +7,22 @@ namespace GitCommands.Git
     {
         /// <summary>
         /// options to git-diff from GE arguments, including artificial commits
-        /// This is an instance class to not have static dependencies in GitModule
         /// </summary>
-        /// <param name="from">The first revision</param>
-        /// <param name="to">The second "current" revision</param>
+        /// <param name="firstRevision">The first revision, "A"</param>
+        /// <param name="secondRevision">The second "current" revision, "B"</param>
         /// <returns></returns>
-        string Get(string from, string to);
+        string Get(string firstRevision, string secondRevision);
+
+        /// <summary>
+        /// options to git-diff from GE arguments, including artificial commits
+        /// </summary>
+        /// <param name="firstRevision">The first revision, "A"</param>
+        /// <param name="secondRevision">The second "current" revision, "B"</param>
+        /// <param name="fileName">The file to compare</param>
+        /// <param name="oldFileName">The old name of the file</param>
+        /// <param name="isTracked">The file is tracked</param>
+        /// <returns></returns>
+        string Get(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked);
     }
 
     /// <summary>
@@ -21,51 +31,92 @@ namespace GitCommands.Git
     /// </summary>
     public sealed class RevisionDiffProvider : IRevisionDiffProvider
     {
+        // This is an instance class to not have static dependencies in GitModule
         private static readonly string StagedOpt = "--cached";
 
         /// <summary>
         /// options to git-diff from GE arguments, including artificial commits
-        /// This is an instance class to not have static dependencies in GitModule
         /// </summary>
-        /// <param name="from">The first revision</param>
-        /// <param name="to">The second "current" revision</param>
+        /// <param name="firstRevision">The first revision</param>
+        /// <param name="secondRevision">The second "current" revision</param>
         /// <returns></returns>
-        public string Get(string from, string to)
+        public string Get(string firstRevision, string secondRevision)
+        {
+            return GetInternal(firstRevision, secondRevision);
+        }
+
+        /// <summary>
+        /// options to git-diff from GE arguments, including artificial commits
+        /// </summary>
+        /// <param name="firstRevision">The first revision, "A"</param>
+        /// <param name="secondRevision">The second "current" revision, "B"</param>
+        /// <param name="fileName">The file to compare</param>
+        /// <param name="oldFileName">The old name of the file</param>
+        /// <param name="isTracked">The file is tracked</param>
+        /// <returns></returns>
+        public string Get(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked)
+        {
+            return GetInternal(firstRevision, secondRevision, fileName, oldFileName, isTracked);
+        }
+
+        private string GetInternal(string firstRevision, string secondRevision, string fileName = null, string oldFileName = null, bool isTracked = true)
         {
             string extra = string.Empty;
-            from = ArtificialToDiffOptions(from);
-            to = ArtificialToDiffOptions(to);
+            firstRevision = ArtificialToDiffOptions(firstRevision);
+            secondRevision = ArtificialToDiffOptions(secondRevision);
 
             //Note: As artificial are options, diff unstage..unstage and 
             // stage..stage will show output, different from e.g. HEAD..HEAD
-            //Diff-to-itself is not always disabled why this is not handled as error in release builds
-            Debug.Assert(!(from == to && (from.IsNullOrEmpty() || from == StagedOpt)),
-                "Unexpectedly two identical artificial revisions to diff: " + from +
+            //Diff-to-itself is not always disabled or is transient why this is not handled as error in release builds
+            Debug.Assert(!(firstRevision == secondRevision && (firstRevision.IsNullOrEmpty() || firstRevision == StagedOpt)),
+                "Unexpectedly two identical artificial revisions to diff: " + firstRevision +
                 ". This will be displayed as diff to HEAD, not an identical diff.");
 
             //As empty (unstaged) and --cached (staged) are options (not revisions),
             // order must be preserved with -R
-            if (from != to && (from.IsNullOrEmpty() ||
-                from == StagedOpt && !to.IsNullOrEmpty()))
+            if (firstRevision != secondRevision && (firstRevision.IsNullOrEmpty() ||
+                               firstRevision == StagedOpt && !secondRevision.IsNullOrEmpty()))
             {
                 extra = "-R";
             }
 
             //Special case: Remove options comparing unstaged-staged
-            if (from.IsNullOrEmpty() && to == StagedOpt ||
-                from == StagedOpt && to.IsNullOrEmpty())
+            if (firstRevision.IsNullOrEmpty() && secondRevision == StagedOpt ||
+                firstRevision == StagedOpt && secondRevision.IsNullOrEmpty())
             {
-                from = to = string.Empty;
+                firstRevision = secondRevision = string.Empty;
             }
 
             //Reorder options - not strictly required
-            if (to == StagedOpt)
+            if (secondRevision == StagedOpt)
             {
                 extra += " " + StagedOpt;
-                to = String.Empty;
+                secondRevision = String.Empty;
             }
 
-            return string.Join(" ", extra, from, to).Trim();
+            if (fileName.IsNullOrWhiteSpace())
+            {
+                extra = string.Join(" ", extra, firstRevision, secondRevision);
+            }
+            else
+            {
+                //Untracked files can only be compared to /dev/null
+                //The UI should normall only allow this for unstaged to staged, but it can be included in multi selections
+                if (!isTracked)
+                {
+                    extra += " --no-index";
+                    oldFileName = fileName;
+                    fileName = "/dev/null";
+                }
+                else
+                {
+                    extra += " " + firstRevision + " " + secondRevision;
+                }
+
+                extra += " -- " + fileName.QuoteNE() + " " + oldFileName.QuoteNE();
+            }
+
+            return extra.Trim();
         }
 
         /// <summary>
@@ -77,13 +128,26 @@ namespace GitCommands.Git
         /// <returns></returns>
         private string ArtificialToDiffOptions(string rev)
         {
-            if (rev.IsNullOrEmpty() || rev == GitRevision.UnstagedGuid) { rev = string.Empty; }
-            else if (rev == "^" || rev == GitRevision.UnstagedGuid + "^" || rev == GitRevision.IndexGuid) { rev = StagedOpt; }
+            if (rev.IsNullOrEmpty() || rev == GitRevision.UnstagedGuid)
+            {
+                rev = string.Empty;
+            }
+            else if (rev == "^" || rev == GitRevision.UnstagedGuid + "^" || rev == GitRevision.IndexGuid)
+            {
+                rev = StagedOpt;
+            }
             else
             {
                 //Normal commit
-                if (rev == "^^" || rev == GitRevision.UnstagedGuid + "^^" || rev == GitRevision.IndexGuid + "^") { rev = "HEAD"; }
-                else if (rev == "^^^" || rev == GitRevision.UnstagedGuid + "^^^" || rev == GitRevision.IndexGuid + "^^") { rev = "HEAD^"; }
+                if (rev == "^^" || rev == GitRevision.UnstagedGuid + "^^" || rev == GitRevision.IndexGuid + "^")
+                {
+                    rev = "HEAD";
+                }
+                else if (rev == "^^^" || rev == GitRevision.UnstagedGuid + "^^^" || rev == GitRevision.IndexGuid + "^^")
+                {
+                    rev = "HEAD^";
+                }
+
                 rev = rev.QuoteNE();
             }
 

GitUI/CommandsDialogs/FormCommit.cs
@@ -2019,7 +2019,7 @@ namespace GitUI.CommandsDialogs
         {
             foreach (var item in items)
             {
-                string output = Module.OpenWithDifftool(item.Name, null, from, to);
+                string output = Module.OpenWithDifftool(item.Name, null, from, to, "", item.IsTracked);
                 if (!string.IsNullOrEmpty(output))
                     MessageBox.Show(this, output);
             }

GitUI/CommandsDialogs/FormDiff.cs
@@ -137,7 +137,7 @@ namespace GitUI.CommandsDialogs
 
             foreach (var selectedItem in DiffFiles.SelectedItems)
             {
-                RevisionGrid.OpenWithDifftool(selectedItem.Name, selectedItem.OldName, diffKind);
+                RevisionGrid.OpenWithDifftool(selectedItem.Name, selectedItem.OldName, diffKind, selectedItem.IsTracked);
             }
         }
 

GitUI/CommandsDialogs/RevisionDiff.cs
@@ -259,13 +259,13 @@ namespace GitUI.CommandsDialogs
 
         private void ShowSelectedFileDiff()
         {
-            if (DiffFiles.SelectedItem == null)
+            var items = _revisionGrid.GetSelectedRevisions();
+            if (DiffFiles.SelectedItem == null || items.Count() == 0)
             {
                 DiffText.ViewPatch("");
                 return;
             }
 
-            var items = _revisionGrid.GetSelectedRevisions();
             if (items.Count() == 1)
             {
                 items.Add(new GitRevision(Module, DiffFiles.SelectedItemParent));
@@ -516,7 +516,7 @@ namespace GitUI.CommandsDialogs
             foreach (var itemWithParent in DiffFiles.SelectedItemsWithParent)
             {
                 GitItemStatus selectedItem = itemWithParent.Item;
-                _revisionGrid.OpenWithDifftool(selectedItem.Name, selectedItem.OldName, diffKind);
+                _revisionGrid.OpenWithDifftool(selectedItem.Name, selectedItem.OldName, diffKind, itemWithParent.Item.IsTracked);
             }
         }
 

GitUI/Editor/FileViewer.cs
@@ -482,12 +482,14 @@ namespace GitUI.Editor
 
         public void ViewGitItemRevision(string fileName, string guid)
         {
-            if (guid == GitRevision.UnstagedGuid) //working directory changes
+            if (guid == GitRevision.UnstagedGuid)
             {
+                //No blob exists for unstaged, present contents from file system
                 ViewFile(fileName);
             }
             else
             {
+                //Retrieve blob, same as GitItemStatus.TreeGuid
                 string blob = Module.GetFileBlobHash(fileName, guid);
                 ViewGitItem(fileName, blob);
             }
@@ -549,6 +551,7 @@ namespace GitUI.Editor
                                 PictureBox.Image = image;
                             });
             }
+            //Check binary from extension/attributes (a secondary check for file contents before display)
             else if (IsBinaryFile(fileName))
             {
                 ViewText(null, "Binary file: " + fileName);

UnitTests/GitCommandsTests/Git/RevisionDiffProviderTest.cs
@@ -27,66 +27,108 @@ namespace GitCommandsTests.Git
         [TestCase(null)]
         [TestCase("")]
         [TestCase(GitRevision.UnstagedGuid)]
-        public void RevisionDiffProvider_should_return_empty_if_To_is_UnstagedGuid(string from)
+        public void RevisionDiffProvider_should_return_empty_if_To_is_UnstagedGuid(string firstRevision)
         {
-            _revisionDiffProvider.Get(from, GitRevision.UnstagedGuid).Should().BeEmpty();
+            _revisionDiffProvider.Get(firstRevision, GitRevision.UnstagedGuid).Should().BeEmpty();
         }
 
         //Two staged revisions gives duplicated options, no reason to clean
         [TestCase("^")]
         [TestCase(GitRevision.IndexGuid)]
-        public void RevisionDiffProvider_should_return_cached_if_both_IndexGuid(string from)
+        public void RevisionDiffProvider_should_return_cached_if_both_IndexGuid(string firstRevision)
         {
-            _revisionDiffProvider.Get(from, GitRevision.IndexGuid).Should().Be("--cached --cached");
+            _revisionDiffProvider.Get(firstRevision, GitRevision.IndexGuid).Should().Be("--cached --cached");
         }
 #endif
 
         [TestCase(GitRevision.IndexGuid, GitRevision.UnstagedGuid)]
         [TestCase("^", "")]
         [TestCase(GitRevision.IndexGuid, null)]
-        public void RevisionDiffProvider_staged_to_unstaged(string from, string to)
+        public void RevisionDiffProvider_staged_to_unstaged(string firstRevision, string secondRevision)
         {
-            _revisionDiffProvider.Get(from, to).Should().BeEmpty();
+            _revisionDiffProvider.Get(firstRevision, secondRevision).Should().BeEmpty();
         }
 
         [TestCase(GitRevision.UnstagedGuid, GitRevision.IndexGuid)]
         [TestCase("", "^")]
-        public void RevisionDiffProvider_unstaged_to_staged(string from, string to)
+        public void RevisionDiffProvider_unstaged_to_staged(string firstRevision, string secondRevision)
         {
-            _revisionDiffProvider.Get(from, to).Should().Be("-R");
+            _revisionDiffProvider.Get(firstRevision, secondRevision).Should().Be("-R");
         }
 
         [TestCase(GitRevision.UnstagedGuid + "^^")]
         [TestCase(GitRevision.IndexGuid + "^")]
         [TestCase("HEAD")]
-        public void RevisionDiffProvider_head_to_unstaged(string from)
+        public void RevisionDiffProvider_head_to_unstaged(string firstRevision)
         {
-            _revisionDiffProvider.Get(from, GitRevision.UnstagedGuid).Should().Be("\"HEAD\"");
+            _revisionDiffProvider.Get(firstRevision, GitRevision.UnstagedGuid).Should().Be("\"HEAD\"");
         }
 
         [TestCase(GitRevision.IndexGuid + "^", "^")]
         [TestCase("HEAD", GitRevision.IndexGuid)]
-        public void RevisionDiffProvider_head_to_staged(string from, string to)
+        public void RevisionDiffProvider_head_to_staged(string firstRevision, string secondRevision)
         {
-            _revisionDiffProvider.Get(from, to).Should().Be("--cached \"HEAD\"");
+            _revisionDiffProvider.Get(firstRevision, secondRevision).Should().Be("--cached \"HEAD\"");
         }
 
         [TestCase(GitRevision.IndexGuid, "HEAD")]
-        public void RevisionDiffProvider_staged_to_head(string from, string to)
+        public void RevisionDiffProvider_staged_to_head(string firstRevision, string secondRevision)
         {
-            _revisionDiffProvider.Get(from, to).Should().Be("-R --cached \"HEAD\"");
+            _revisionDiffProvider.Get(firstRevision, secondRevision).Should().Be("-R --cached \"HEAD\"");
         }
 
         [TestCase("HEAD", "123456789")]
-        public void RevisionDiffProvider_normal1(string from, string to)
+        public void RevisionDiffProvider_normal1(string firstRevision, string secondRevision)
         {
-            _revisionDiffProvider.Get(from, to).Should().Be("\"HEAD\" \"123456789\"");
+            _revisionDiffProvider.Get(firstRevision, secondRevision).Should().Be("\"HEAD\" \"123456789\"");
         }
 
         [TestCase("123456789", "HEAD")]
-        public void RevisionDiffProvider_normal2(string from, string to)
+        public void RevisionDiffProvider_normal2(string firstRevision, string secondRevision)
         {
-            _revisionDiffProvider.Get(from, to).Should().Be("\"123456789\" \"HEAD\"");
+            _revisionDiffProvider.Get(firstRevision, secondRevision).Should().Be("\"123456789\" \"HEAD\"");
+        }
+
+        //Standard usage when filename is included
+        [TestCase("123456789", GitRevision.UnstagedGuid, "a.txt", null, true)]
+        public void RevisionDiffProvider_fileName_tracked(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked)
+        {
+            _revisionDiffProvider.Get(firstRevision, secondRevision, fileName, oldFileName, isTracked).Should().Be("\"123456789\"  -- \"a.txt\"");
+        }
+
+        //If fileName is null, ignore oldFileName and tracked
+        [TestCase("123456789", "HEAD", null, "b.txt", true)]
+        public void RevisionDiffProvider_fileName_null_with_oldname(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked)
+        {
+            _revisionDiffProvider.Get(firstRevision, secondRevision, fileName, oldFileName, isTracked).Should().Be("\"123456789\" \"HEAD\"");
+        }
+
+        //Include old filename if is included
+        [TestCase("123456789", "234567890", "a.txt", "b.txt", true)]
+        public void RevisionDiffProvider_fileName_oldfilename(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked)
+        {
+            _revisionDiffProvider.Get(firstRevision, secondRevision, fileName, oldFileName, isTracked).Should().Be("\"123456789\" \"234567890\" -- \"a.txt\" \"b.txt\"");
+        }
+
+        //normal testcase when untracked is set
+        [TestCase(GitRevision.IndexGuid, GitRevision.UnstagedGuid, "a.txt", null, false)]
+        public void RevisionDiffProvider_fileName_untracked(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked)
+        {
+            _revisionDiffProvider.Get(firstRevision, secondRevision, fileName, oldFileName, isTracked).Should().Be("--no-index -- \"/dev/null\" \"a.txt\"");
+        }
+
+        //If fileName is null, ignore oldFileName and tracked
+        [TestCase(GitRevision.IndexGuid, GitRevision.UnstagedGuid, null, "b.txt", false)]
+        public void RevisionDiffProvider_fileName_null_Untracked(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked)
+        {
+            _revisionDiffProvider.Get(firstRevision, secondRevision, fileName, oldFileName, isTracked).Should().BeEmpty();
+        }
+
+        //Ignore revisions for untracked
+        [TestCase("123456789", "234567890", "a.txt", "b.txt", false)]
+        public void RevisionDiffProvider_fileName_oldfilename_Untracked(string firstRevision, string secondRevision, string fileName, string oldFileName, bool isTracked)
+        {
+            _revisionDiffProvider.Get(firstRevision, secondRevision, fileName, oldFileName, isTracked).Should().Be("--no-index -- \"/dev/null\" \"a.txt\"");
         }
     }
 }

commit d38667a981bab1063c5554fa93ab324b0d1ec95c

Difference for initial commit of a file were presented as a diff to the working directory
Fixes #4580
Regression from #4313

Non-existing commits are handled with the "revision" null which is mostly handled as a working directory file.
The compare options for working directory files were limited in 2.50. In 2.51 the changes are presented with git-diff if possible and the non-existing parent were incorrectly handled.

GitUI/GitUIExtensions.cs
@@ -88,6 +88,15 @@ namespace GitUI
         {
             return diffViewer.ViewPatch(() =>
             {
+                if (firstRevision == null && secondRevision.IsNotNullOrWhitespace())
+                {
+                    // The previous commit does not exist, nothing to compare with
+                    if (file.TreeGuid.IsNullOrEmpty())
+                        diffViewer.ViewGitItemRevision(file.Name, secondRevision);
+                    else
+                        diffViewer.ViewGitItem(file.Name, file.TreeGuid);
+                }
+
                 string selectedPatch = diffViewer.GetSelectedPatch(firstRevision, secondRevision, file);
                 return selectedPatch ?? defaultText;
             });